31d693040b9a78e8499bfbb6b61300d15fac0829
Kencliff <etangkencliffandocktabiatem@gmail.com>
2022-07-29 13:18:22 +0100

    portfolio

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import CTA from './CTA';
+import HeaderSocials from './HeaderSocials';
+import './header.css';
+import ME from '../../assets/me.jpg';
+
+const Header = () => {
+  return (
+    <header id="home">
+      <div className="container header__container border-box">
+        <div className="row">
+          <div className='col-12 col-md-6 col-lg-6'>
+            <h5>Hello I'm,</h5>
+            <h1>Etang Kencliff Andock</h1>
+            <h4 className="text-light-primary">Full-stack Web Developer</h4>
+            <CTA />
+            <HeaderSocials />
+          </div>
+          
+          <div className="my-image col-12 col-md-6 col-lg-6">
+            <img src={ME} alt="me"/>
+          </div>
+        </div>
+      </div>
+        
+      
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub, FaTwitter } from 'react-icons/fa'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <small>Social Links: </small>
+      <a href="https://www.linkedin.com/in/etang-kencliff-andock-tabi-atem-085226240/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
+      <a href="https://github.com/Etang-ken" target="_blank" rel="noreferrer" ><FaGithub /></a>
+      <a href="https://www.twitter.com/Ekencliff" target="_blank" rel="noreferrer"><FaTwitter /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
